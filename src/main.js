74f724022a0a53f6d13593321cdd2709110c3a51
David Peter <mail@david-peter.de>
2024-02-18 22:02:38 +0100

    Better history implementation

--- src/main.js
+++ src/main.js
@@ -8,18 +8,18 @@ let button_units_el;
 let button_constants_el;
 
 async function calculate() {
-  let result = await invoke("calculate", { query: query_el.value, updateContext: false });
-  console.log(result);
-  if (result.output == "") {
-    current_el.innerHTML = "&nbsp;";
-  } else {
-    current_el.innerHTML = result.output;
-  }
-  if (result.is_error) {
-    query_el.classList.add("query_error");
-  } else {
-    query_el.classList.remove("query_error");
-  }
+    let result = await invoke("calculate", { query: query_el.value, updateContext: false });
+    console.log(result);
+    if (result.output == "") {
+        current_el.innerHTML = "&nbsp;";
+    } else {
+        current_el.innerHTML = result.output;
+    }
+    if (result.is_error) {
+        query_el.classList.add("query_error");
+    } else {
+        query_el.classList.remove("query_error");
+    }
 }
 
 async function submit() {
@@ -30,36 +30,36 @@ async function submit() {
         return;
     }
 
-    history_el.innerHTML = "<div class=\"history_item\" data-result=\"" + encodeURIComponent(result.value) + "\">"+ result.output + "</div>" + history_el.innerHTML;
-    current_el.innerHTML = "&nbsp;";
-    query_el.value = "";
+    let history_entry = document.createElement("div");
+    history_entry.innerHTML = result.output;
+    history_entry.classList.add("history_item");
+    history_el.appendChild(history_entry);
 
-    // Attach event to new .history_item elements
-    let history_items = document.querySelectorAll(".history_item");
-    history_items.forEach((el) => {
-      el.addEventListener("click", (e) => {
-        query_el.value = decodeURIComponent(el.attributes["data-result"].value);
+    history_entry.addEventListener("click", (e) => {
+        query_el.value = result.value;
         calculate();
         // focus on query_el, last character:
         query_el.focus();
         query_el.setSelectionRange(query_el.value.length, query_el.value.length);
-      });
     });
-  }
+
+    current_el.innerHTML = "&nbsp;";
+    query_el.value = "";
+}
 
 window.addEventListener("DOMContentLoaded", () => {
-  query_form_el = document.querySelector("#query_form");
-  query_el = document.querySelector("#query");
-  current_el = document.querySelector("#current");
-  history_el = document.querySelector("#history");
+    query_form_el = document.querySelector("#query_form");
+    query_el = document.querySelector("#query");
+    current_el = document.querySelector("#current");
+    history_el = document.querySelector("#history");
 
-  query_el.addEventListener("input", (e) => {
-    calculate();
-  });
+    query_el.addEventListener("input", (e) => {
+        calculate();
+    });
 
-  query_form_el.addEventListener("submit", (e) => {
-    e.preventDefault();
-    submit();
-  });
+    query_form_el.addEventListener("submit", (e) => {
+        e.preventDefault();
+        submit();
+    });
 
 });
